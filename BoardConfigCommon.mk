19b93b15b6340bba32f6faa7dbc1f1dc888fda79
Kevin Mark <kmark937@gmail.com>
2019-06-12 05:19:30 -0400

    Legacy SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,7 @@ USE_DEVICE_SPECIFIC_DATASERVICES := true
 TARGET_USE_SDCLANG := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Wifi module
